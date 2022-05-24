c7b8ad952563c90782e0f115febc0a4abc18b98f
VictoriaBecher <victoria_enache@ymail.com>
2022-05-24 22:05:44 +0300

    js sedinta 3

---
+++ M01S03/ex05/app.js
@@ -0,0 +1,24 @@
+var person = {
+  name: 'Victoria',
+  surname: 'Becher',
+  age: 36,
+  petOwner: false,
+  skills: ['html', 'javascript', 'css'],
+  friends: [
+    {
+      name: 'Larry',
+      surname: 'Larryson',
+      age: 30,
+    },
+    {
+      name: 'Steven',
+      surname: 'Stevenson',
+      age: 31,
+    },
+    {
+      name: 'Carol',
+      surname: 'Carolson',
+      age: 29,
+    },
+  ],
+};
