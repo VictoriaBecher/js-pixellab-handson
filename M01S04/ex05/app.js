a885f6e16e77648fdd26a7155564e2abb9cfa62c
VictoriaBecher <victoria_enache@ymail.com>
2022-05-31 22:01:51 +0300

    js if for

---
+++ M01S04/ex05/app.js
@@ -0,0 +1,40 @@
+var defaultNumber = 2;
+var defaultWarningMessage = `Nu ai introdus un numar. Folosesc ${defaultNumber}`;
+
+var i = 0;
+var number = prompt('Introdu un numar');
+number = number.trim();
+
+// for (i = 0; i <= 100; i++) {
+//   console.log(i);
+
+//   if (i % 7 !== 0) {
+//     continue;
+//   }
+
+//   console.log('Acest numar este multiplu de 7');
+// }
+
+// Folosind metoda prompt cere utilizatorului numarul pentru care trebuie sa gasesti multipli si afiseaza mesajul: “Acest numar este multiplu de x.”
+
+if (number === null || number.length === 0) {
+  console.warn(defaultWarningMessage);
+  number = defaultNumber;
+} else {
+  number = Number(number);
+}
+
+if (isNaN(number) === true) {
+  console.warn(defaultWarningMessage);
+  number = defaultNumber;
+}
+
+for (i = 6; i <= 100; i++) {
+  console.log(i);
+
+  if (i % number !== 0) {
+    continue;
+  }
+
+  console.log(`Acest numar este multiplu de ${number}`);
+}
---
+++ M01S04/ex06/app.js
@@ -0,0 +1,33 @@
+var person = {
+  name: 'Ana',
+  surname: 'Maria',
+  age: 30,
+  petOwner: false,
+  skills: [
+    'html',
+    'javascript',
+    'css',
+    'java',
+    'c++',
+    'node',
+    'jquery',
+    'node.js',
+  ],
+  friends: [
+    {
+      name: 'Larry',
+      surname: 'Larryson',
+      age: 30,
+    },
+    {
+      name: 'Steven',
+      surname: 'Stevenson',
+      age: 30,
+    },
+    {
+      name: 'Carol',
+      surname: 'Carolson',
+      age: 30,
+    },
+  ],
+};
