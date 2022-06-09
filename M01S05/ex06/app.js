92c524daba9b387bcfcc62b06c28b26f42470234
VictoriaBecher <victoria_enache@ymail.com>
2022-06-09 22:00:29 +0300

    js functii

---
+++ M01S05/ex06/app.js
@@ -0,0 +1,21 @@
+const person = {
+  getName: function () {
+    return 'Ana Maria';
+  },
+  getAge: function () {
+    return 35;
+  },
+};
+
+function accessor(methodSuffix) {
+  const methodName = `get${methodSuffix}`;
+
+  // return person.getName
+  // return person.getAge
+  return person[methodName]();
+}
+
+console.warn(`Folosind accesorul salveaza numele mic al persoanei intr-o variabila si foloseste-l in propozitia: “Eu sunt xxx.”
+`);
+const personFirstName = accessor('Name');
+console.log(`Eu sunt ${personFirstName}.`);
