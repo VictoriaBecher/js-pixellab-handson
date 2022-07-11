b6fb63218aa78724e3d490a6ef0266a6f4e08318
VictoriaBecher <victoria_enache@ymail.com>
2022-07-11 16:48:08 +0300

    avarii

--- M02S01/ex04/app.js
+++ M02S01/ex04/app.js
@@ -1,4 +1,8 @@
 class Car {
+  areLightsOn = false;
+  intervalID = 0;
+  initialLightState = false;
+
   constructor(positionX, positionY, color) {
     this.positionX = positionX;
     this.positionY = positionY;
@@ -44,34 +48,83 @@ class Car {
     this.hubCapBack = document.createElement('div');
     this.hubCapBack.classList.add('wheel__cap');
     this.wheelBack.append(this.hubCapBack);
+
+    return this;
   }
 
-  areHazardsOn = false;
+  // areHazardsOn = false;
+
+  // turnLightsOn() {
+  //   this.lightBack.style.backgroundColor = 'red';
+  //   this.lightFront.style.backgroundColor = 'red';
+  // }
+
+  // turnLightsOff() {
+  //   this.lightBack.style.backgroundColor = 'white';
+  //   this.lightFront.style.backgroundColor = 'white';
+  // }
+
+  // toggleHazards() {
+  //   this.interval = setInterval(() => {
+  //     this.turnLightsOn();
+
+  //     setTimeout(() => {
+  //       this.turnLightsOff();
+  //     }, 1000);
+  //   }, 2000);
+  //   this.areHazardsOn = true;
+  // }
+
+  // stopHazards() {
+  //   clearInterval(this.interval);
+  //   this.areHazardsOn = false;
+  // }
 
   turnLightsOn() {
     this.lightBack.style.backgroundColor = 'red';
-    this.lightFront.style.backgroundColor = 'red';
+    this.lightFront.style.backgroundColor = 'yellow';
+
+    this.areLightsOn = true;
+
+    return this;
   }
 
   turnLightsOff() {
     this.lightBack.style.backgroundColor = 'white';
     this.lightFront.style.backgroundColor = 'white';
+
+    this.areLightsOn = false;
+
+    return this;
   }
 
   toggleHazards() {
-    this.interval = setInterval(() => {
-      this.turnLightsOn();
+    const self = this;
 
-      setTimeout(() => {
-        this.turnLightsOff();
-      }, 1000);
-    }, 2000);
-    this.areHazardsOn = true;
-  }
+    if (self.intervalID !== 0) {
+      clearInterval(self.intervalID);
+      self.intervalID = 0;
 
-  stopHazards() {
-    clearInterval(this.interval);
-    this.areHazardsOn = false;
+      if (self.initialLightState === true) {
+        self.turnLightsOn();
+      } else {
+        self.turnLightsOff();
+      }
+
+      return;
+    }
+
+    self.initialLightState = self.areLightsOn;
+
+    self.intervalID = setInterval(function () {
+      if (self.areLightsOn === true) {
+        self.turnLightsOff();
+      } else {
+        self.turnLightsOn();
+      }
+    }, 500);
+
+    return self;
   }
 
   moveTo(positionX, positionY) {
@@ -79,14 +132,20 @@ class Car {
     this.positionY = positionY;
     this.frame.style.left = `${this.positionX}px`;
     this.frame.style.top = `${this.positionY}px`;
+
+    return this;
   }
 
   engageBreak() {
     this.lightBack.style.backgroundColor = 'red';
+
+    return this;
   }
 
   disengageBreak() {
     this.lightBack.style.backgroundColor = 'white';
+
+    return this;
   }
 
   changeTireColor(color) {
@@ -101,11 +160,15 @@ class Car {
 
   render() {
     document.body.append(this.frame);
+
+    return this;
   }
 }
-const car01 = new Car(140, 140, 'teal');
-car01.render();
-car01.moveTo(400, 500);
+// const car01 = new Car(140, 140, 'teal');
+// car01.render();
+// car01.moveTo(400, 500);
+
+const car01 = new Car(300, 16, 'teal').render().turnLightsOn();
 
 const car02 = new Car(200, 300, '#1C8BEF');
 car02.render();
