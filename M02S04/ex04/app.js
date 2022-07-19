b23dafa9f185997d84251c4525547129063af2e0
VictoriaBecher <victoria_enache@ymail.com>
2022-07-19 21:59:48 +0300

    hero, pristine (validation), glide

---
+++ M02S04/ex04/app.js
@@ -0,0 +1,36 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const personForm = document.querySelector('#personForm');
+  const submitButton = personForm.querySelector('button[type="submit"]');
+
+  const pristine = new Pristine(personForm);
+  const isValid = pristine.validate(true);
+
+  console.log(isValid);
+  // personForm.addEventListener('submit', (event) => {
+  //   event.preventDefault();
+
+  //   const valid = pristine.validate();
+  // });
+
+  if (isValid === false) {
+    submitButton.disabled = true;
+  }
+
+  personForm.addEventListener('input', (event) => {
+    const formControl = event.target;
+
+    if (formControl.name === undefined || formControl.name.length <= 0) {
+      return;
+    }
+
+    const isValid = pristine.validate(true);
+    // daca isValid => disabled = false
+    //  if !isValid => disabled = true
+    submitButton.disabled = !isValid;
+  });
+
+  personForm.addEventListener('submit', (event) => {
+    event.preventDefault;
+    // do ajax stuff
+  });
+});
